b74b97c6d5c31d5127cf79c29e739aad68c39bab
Ahmed Shaban <ahmed.shaban.sh@gmail.com>
2018-09-10 09:44:43 +0200

    modified main driver

--- Basic Algorithms implementation in cpp/main.cpp
+++ Basic Algorithms implementation in cpp/main.cpp
@@ -8,11 +8,10 @@
 int main()
 {
 	//driver for testing searching algorithms. 
-	std::vector<double> vec{ 10, 5, 6, 8, 9, 5, 6, 1, 2, 22, 45, 7, 10 }; 
-	searching_algorithms algorithm; 
-	double key; 
-	std::cout << "Enter key: \n"; 
-	std::cin >> key; 
-	algorithm.time_comparison(vec, key); 
+	searching_algorithms algorithm;
+	std::vector<double> vec(1000);
+	std::generate(vec.begin(), vec.end(), std::rand);
+	double key = *(vec.cend() - 1);
+	algorithm.time_comparison(vec, key);
 	return EXIT_SUCCESS;
 }
